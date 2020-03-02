4525a2eb70ff0ae359bdc7be061d3272edd84755
LazarPopadic97 <LazarPopadic97>
2020-03-02 10:24:14 +0100

    dodat interfejs SpecifikacijaInterfejs kao i 3 metode

--- Menjacnica/src/specifikacijaInterfejs/SpecifikacijaInterfejs.java
+++ Menjacnica/src/specifikacijaInterfejs/SpecifikacijaInterfejs.java
@@ -1,5 +1,11 @@
 package specifikacijaInterfejs;
 
+import java.util.GregorianCalendar;
+
 public interface SpecifikacijaInterfejs {
 
+	void dodajKurs(double kurs,GregorianCalendar datum);
+	void obrisiKurs(double kurs,GregorianCalendar datum);
+	double pronadjiKurs(GregorianCalendar datum);
+	
 }
